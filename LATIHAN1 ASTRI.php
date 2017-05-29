bc81cbfa55dbfe9eb12767a62763e7a80761685d
Astriningsih04 <astriningsih109@gmail.com>
2017-05-29 15:31:33 +0700

    Add files via upload

---
+++ LATIHAN1 ASTRI.php
@@ -0,0 +1,34 @@
+ <html>
+<title>Menghitung Luas Persegi Panjang dan Segitiga</title>
+<body>
+<a><b> (2010.000.007) </b></a><br/>
+<br>
+<br>
+<a><b> A) Rumus Luas persegi panjang adalah P(panjang) x L(Lebar)</b></a><br/>
+<?php
+$panjang = 4;
+$lebar = 20;
+$hasil =$panjang *$lebar;
+echo"Luas persegi panjang :<br/>";
+echo"diketahui panjang =$panjang <br/>";
+echo"diketahui lebar =$lebar <br/>";
+echo"L = P x L <br/>";
+echo" $panjang x $lebar =$hasil <br/>";
+echo" Jadi Luas Persegi Panjang adalah $hasil <br/>";
+?>
+<br>
+<br>
+<a><b> B) Rumus Luas segitiga adalah 1/2 x A(alas) x T(tinggi)</b></a><br/>
+<?php
+$alas = 20;
+$tinggi = 7;
+$hasil =1/2*$alas *$tinggi;
+echo"Luas Segitiga <br/>";
+echo"Diketahui Alas =$alas <br/>";
+echo"Diketahui tinggi =$tinggi <br/>";
+echo"L = A x T x 1/2 <br/>";
+echo" $alas x $tinggi x 1/2 = $hasil <br/>";
+echo"jadi Luas Segitiga adalah $hasil";
+?>
+</body>
+</html>
